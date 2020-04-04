258ce76aa298d132cc2938f27d4d0c472cd000d9
mikehwagz <mike@selfaware.studio>
2020-04-04 11:18:37 -0400

    refactor(web): clean up

--- web/src/util/client.js
+++ web/src/util/client.js
@@ -1,7 +1,8 @@
 const client = require('@sanity/client')
 
 module.exports = client({
-  projectId: '<PROJECT_ID_HERE>',
+  // TODO: set sanity projectId
+  projectId: '0v76nywm',
   dataset: 'production',
   useCdn: true,
 })
