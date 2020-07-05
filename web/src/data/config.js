5e7b3af51cc2abc0fd3f4432a49105d1f4c2b90e
mikehwagz <mike@selfaware.studio>
2020-07-05 01:48:36 -0400

    feat(web): update html head and add default config 11ty data file

---
+++ web/src/data/config.js
@@ -0,0 +1,11 @@
+// const client = require('../util/client.js')
+// const groq = require('groq')
+
+// module.exports = async function() {
+//   return await client.fetch(groq`*[_type == 'config'][0] {
+//     seo {
+//       ...,
+//       image { ...image.asset->, alt }
+//     }
+//   }`)
+// }
